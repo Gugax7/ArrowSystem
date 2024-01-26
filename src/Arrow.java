cc3183a34064cee31175f6aada9226865c02958e
Gustavo Salmazo <gux.salmazo@gmail.com>
2024-01-26 14:06:07 -0300

    some improves and tests on code

--- src/Arrow.java
+++ src/Arrow.java
@@ -2,42 +2,56 @@ public class Arrow {
     private double xSpeed;
     private double ySpeed;
     private double weight;
-    private double tSpeed;
-    private double angleInDegrees = -1;
+    private double totalSpeed;
+    private double angleOfThrow;
+
 
     public Arrow(double xSpeed, double ySpeed, double weight) {
         this.xSpeed = xSpeed;
         this.ySpeed = ySpeed;
         this.weight = weight;
+        this.angleOfThrow = angleInDegrees();
+        this.totalSpeed = calculateTotalSpeed();
     }
 
     public double getAngleInDegrees() {
-        return angleInDegrees;
+        return angleOfThrow;
     }
 
-    public Arrow(double angleInDegrees, double tSpeed){
-        this.tSpeed = tSpeed;
-        this.angleInDegrees = angleInDegrees;
-        this.weight = 20; // default weight, I will change it later.
-        xSpeed = tSpeed * Math.cos(Math.toRadians(angleInDegrees));
-        ySpeed = tSpeed * Math.sin(Math.toRadians(angleInDegrees));
+    public Arrow(double angleInDegrees, double totalSpeed){
+        this.totalSpeed = totalSpeed;
+        this.angleOfThrow = angleInDegrees;
+        xSpeed = totalSpeed * Math.cos(Math.toRadians(angleInDegrees));
+        ySpeed = totalSpeed * Math.sin(Math.toRadians(angleInDegrees));
     }
     public double distanceTraveled(){
         double time = this.ySpeed / 5;
         return xSpeed*time;
     }
-    public double totalSpeed(){
+    private double calculateTotalSpeed(){
         return Math.sqrt((xSpeed*xSpeed)+(ySpeed*ySpeed));
     }
     public double impulseFromBow(){
-        return totalSpeed() * weight;
+        return totalSpeed * weight;
     }
-    public double angleOfThrow(){
-        if(angleInDegrees != -1){
-            return angleInDegrees;
-        }
+    private double angleInDegrees(){
         double tan = ySpeed/xSpeed;
         double radians = Math.atan(tan);
         return Math.toDegrees(radians);
+    }
+
+    public double getAngleOfThrow() {
+        return angleOfThrow;
+    }
+    public double getTotalSpeed(){
+        return totalSpeed;
+    }
+
+    public double getXSpeed() {
+        return xSpeed;
+    }
+
+    public double getYSpeed() {
+        return ySpeed;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,18 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Arrow arrow = new Arrow(5*Math.sqrt(3),5,20);
-        Arrow arrowWithAngle = new Arrow(30,10);
+        Arrow arrow = new Arrow(20,20,20);
+        Arrow arrowWithAngle = new Arrow(45,20);
+        // arrow with Angle to its to put a weight in arrow with angle, i dont know yet how i can improve this.
+        Arrow arrowWithAngle2 = new Arrow(arrowWithAngle.getXSpeed(),arrowWithAngle.getYSpeed(),40);
+        //some tests
         System.out.println("The arrow had reached " + arrow.distanceTraveled() + " meters");
         System.out.println("The impulse that bow caused in the arrow was " + arrow.impulseFromBow() + " Ns");
-        System.out.println("Total speed of arrow was " + arrow.totalSpeed() + " m/s");
-        System.out.println(arrow.angleOfThrow());
+        System.out.println("Total speed of arrow was " + arrow.getTotalSpeed() + " m/s");
+        System.out.println(arrow.getAngleOfThrow());
         System.out.println("The arrow had reached " + arrowWithAngle.distanceTraveled() + " meters");
-        System.out.println("The impulse that bow caused in the arrow was " + arrowWithAngle.impulseFromBow() + " Ns");
-        System.out.println("Total speed of arrow was " + arrowWithAngle.totalSpeed() + " m/s");
-        System.out.println(arrowWithAngle.angleOfThrow());
+        System.out.println("The impulse that bow caused in the arrow was " + arrowWithAngle2.impulseFromBow() + " Ns");
+        System.out.println("Total speed of arrow was " + arrowWithAngle.getTotalSpeed() + " m/s");
+        System.out.println(arrowWithAngle.getAngleOfThrow());
     }
 }
